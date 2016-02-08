ab6923e832f5c7ac401f0181f3408568358bbfa4
Naman Gupta <namangt68@gmail.com>
2016-02-09 04:10:28 +0530

    first commit

---
+++ R/uploaddata.R
@@ -0,0 +1,11 @@
+uploaddata <- function(csvfile, ...){
+  if(substring(tolower(csvfile), nchar(csvfile)-3) != ".csv"){
+    stop('Uploaded data needs to be .csv file. When using MS Excel, try "Save As" and select type "CSV (comma delimited)"');
+  }
+  
+  #read csv data
+  mydata <- read.csv(csvfile, row.names = NULL, stringsAsFactors=FALSE, ...);
+
+  #return dataset
+  return(mydata)  
+}
